fbe816510636e11cf2aafe496ed1e96430cb5d5c
DalexHD <eldalexhd@gmail.com>
2021-06-25 14:53:48 +0000

    feat: add changes.

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import { hotjar } from 'react-hotjar';
 
 if (process.browser) {
   hotjar.initialize(2473131, 6);
-  hotjar.identify('USER_ID', { userProperty: 'value' });
 }
 
 function MyApp({ Component, pageProps }) {
