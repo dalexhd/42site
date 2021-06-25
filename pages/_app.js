4cb3625285b34b0e333c20b5fd68fb664634bde8
DalexHD <eldalexhd@gmail.com>
2021-06-25 14:53:16 +0000

    test.

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,10 @@ import '../styles/globals.css'
 
 import { hotjar } from 'react-hotjar';
 
-hotjar.initialize(2473131, 6);
-hotjar.identify('USER_ID', { userProperty: 'value' });
+if (process.browser) {
+  hotjar.initialize(2473131, 6);
+  hotjar.identify('USER_ID', { userProperty: 'value' });
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
